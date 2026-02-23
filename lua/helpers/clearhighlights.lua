2ca0ca1b6acea9924ac45489152bf3828ce4c38d
Alex Lebedev <alexey.broadcast@gmail.com>
2026-02-23 12:33:23 +0300

    dont change active window

--- lua/helpers/clearhighlights.lua
+++ lua/helpers/clearhighlights.lua
@@ -21,17 +21,14 @@ end
 --- Clear highlights for a group in all buffers across all tabpages/windows
 --- @param group_name string highlight group name
 function M.ClearHighlightsInAllBuffers(group_name)
-  local savedWin = vim.api.nvim_get_current_win() 
   for _, tabpage in ipairs(vim.api.nvim_list_tabpages()) do
     local tab_windows = vim.api.nvim_tabpage_list_wins(tabpage)
     for _, win in ipairs(tab_windows) do
-      vim.api.nvim_set_current_win(win)
       for _, match in ipairs(get_matches(group_name)) do
         pcall(vim.fn.matchdelete, match.id)
       end
     end
   end
-  vim.api.nvim_set_current_win(savedWin)
 end
 
 return M
